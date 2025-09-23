e6a1af2ce9df16c86bfe4c610e62e0e3c77a0f93
pedro_silva <pedrohsilva0407@outlook.com>
2025-09-22 22:56:59 -0300

    normal

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-
+import taiwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [
     react(),
-    
+    taiwindcss(),
   ],
   base: '/GourmetON/'
 })
